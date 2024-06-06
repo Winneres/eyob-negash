470c41c1a3847c8339882d36f8c96697f07cc981
Winneres <eyobapro@gmail.com>
2024-06-06 21:19:00 +0300

    Add files via upload

---
+++ eyob/java.js
@@ -0,0 +1,91 @@
+const hamburger = document.getElementById('hamburger'); 
+const menu = document.querySelector('.menu'); 
+
+hamburger.addEventListener('click', function () { 
+	const hamIcon = this.querySelector('.hamburger-icon'); 
+	const crossIcon = this.querySelector('.cross-icon'); 
+	if (hamIcon.style.display === "none") { 
+		hamIcon.style.display = "inline-block"
+		menu.style.display = "none"
+		crossIcon.style.display = "none"
+	} 
+	else { 
+		crossIcon.style.display = "inline-block"
+		hamIcon.style.display = "none"
+		menu.style.display = "block"
+	} 
+});
+(function () {
+	"use strict";
+	/*
+	 * Form Validation
+	 */
+  
+	// Fetch all the forms we want to apply custom validation styles to
+	const forms = document.querySelectorAll(".needs-validation");
+	const result = document.getElementById("result");
+	// Loop over them and prevent submission
+	Array.prototype.slice.call(forms).forEach(function (form) {
+	  form.addEventListener(
+		"submit",
+		function (event) {
+		  if (!form.checkValidity()) {
+			event.preventDefault();
+			event.stopPropagation();
+  
+			form.querySelectorAll(":invalid")[0].focus();
+		  } else {
+			/*
+			 * Form Submission using fetch()
+			 */
+  
+			const formData = new FormData(form);
+			event.preventDefault();
+			event.stopPropagation();
+			const object = {};
+			formData.forEach((value, key) => {
+			  object[key] = value;
+			});
+			const json = JSON.stringify(object);
+			result.innerHTML = "Please wait...";
+  
+			fetch("https://api.web3forms.com/submit", {
+			  method: "POST",
+			  headers: {
+				"Content-Type": "application/json",
+				Accept: "application/json"
+			  },
+			  body: json
+			})
+			  .then(async (response) => {
+				let json = await response.json();
+				if (response.status == 200) {
+				  result.innerHTML = json.message;
+				  result.classList.remove("text-gray-500");
+				  result.classList.add("text-green-500");
+				} else {
+				  console.log(response);
+				  result.innerHTML = json.message;
+				  result.classList.remove("text-gray-500");
+				  result.classList.add("text-red-500");
+				}
+			  })
+			  .catch((error) => {
+				console.log(error);
+				result.innerHTML = "Something went wrong!";
+			  })
+			  .then(function () {
+				form.reset();
+				form.classList.remove("was-validated");
+				setTimeout(() => {
+				  result.style.display = "none";
+				}, 5000);
+			  });
+		  }
+		  form.classList.add("was-validated");
+		},
+		false
+	  );
+	});
+  })();
+  
